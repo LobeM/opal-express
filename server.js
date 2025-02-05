9b4f1afcb850ddb1a6e1ae786086d20637d1e887
Lobe <lobe350@gmail.com>
2025-02-05 15:13:14 +0200

    Add video chunk handling and file upload functionality to server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { Server } = require('socket.io');
+const fs = require('fs');
 const http = require('http');
 const cors = require('cors');
+const { Readable } = require('stream');
 
 require('dotenv').config();
 
@@ -17,8 +19,31 @@ const io = new Server(server, {
   },
 });
 
+let recordedChunks = [];
+
 io.on('connection', (socket) => {
   console.log('🟢 Socket is connected');
+  socket.on('video-chunks', async (data) => {
+    console.log('🟢 Video chunks is sent');
+    const writestream = fs.createWriteStream('temp_upload/' + data.filename);
+    recordedChunks.push(data.chunks);
+    const videoBlob = new Blob(recordedChunks, {
+      type: 'video/webm; codecs=vp9',
+    });
+    const buffer = Buffer.from(await videoBlob.arrayBuffer());
+    const readStream = Readable.from(buffer);
+    readStream.pipe(writestream).on('finish', () => {
+      console.log('🟢 Chunk Saved');
+    });
+  });
+
+  socket.on('process-video', async (data) => {
+    console.log('🟢 Processing video...');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🟢 Socket.id', socket.id, 'is disconnected');
+  });
 });
 
 server.listen(5000, () => {
