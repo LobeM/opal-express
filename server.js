b91d03fb4b5d48c66257054dede9ba9cc6e10ef7
Lobe <lobe350@gmail.com>
2025-02-10 16:57:29 +0200

    Send filename in processing API request

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ io.on('connection', (socket) => {
     recordedChunks = [];
     fs.readFile('temp_upload/' + data.filename, async (err, file) => {
       const processing = await axios.post(
-        `${process.env.NEXT_API_HOST}recording/${data.userId}/processing`
+        `${process.env.NEXT_API_HOST}recording/${data.userId}/processing`,
+        { filename: data.filename }
       );
       if (processing.data.status !== 200)
         return console.log(
